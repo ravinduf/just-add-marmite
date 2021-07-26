6d32b380b3d972168bfb0b30beeedbc3ec6c6ba1
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-07-26 10:12:57 +0530

    added code to get data from contentful

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,23 @@
-export default function Recipes() {
+import { createClient } from 'contentful';
+
+export const getStaticProps = async () => {
+
+  const client = createClient({
+    space: process.env.CONTENTFULL_SPACE_ID,
+    accessToken: process.env.CONTENTFULL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: 'recipe' });
+
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
   return (
     <div className="recipe-list">
       Recipe List
