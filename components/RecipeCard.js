4b3c084d04b915a6d552ada45490d8b2f1df89e3
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-08-26 08:24:27 +0530

    fetched images from contentfull

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,13 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const RecipeCard = ({ recipe }) => {
 
   const { title, slug, cookingTime, thumbnail } = recipe.fields;
-
+  console.log(`https:${thumbnail.fields.file.url}`)
     return (
         <div className="card">
           <div className="featured">
             {/* image - thumb */}
+            <Image src={`https:${thumbnail.fields.file.url}`} 
+              width={thumbnail.fields.file.details.image.width} 
+              height={thumbnail.fields.file.details.image.height}
+            />
           </div>
           <div className="content">
             <div className="info">
