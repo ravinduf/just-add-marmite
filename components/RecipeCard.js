2f5a3f57aa9760f67356a4999234d1bc6baaade7
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-08-26 20:10:34 +0530

    added styles

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -3,8 +3,9 @@ import Image from 'next/image';
 
 const RecipeCard = ({ recipe }) => {
 
-  const { title, slug, cookingTime, thumbnail } = recipe.fields;
-  console.log(`https:${thumbnail.fields.file.url}`)
+  const { title, slug, cookingTIme, thumbnail } = recipe.fields;
+  console.log( cookingTIme )
+  // console.log(`https:${thumbnail.fields.file.url}`)
     return (
         <div className="card">
           <div className="featured">
@@ -17,7 +18,7 @@ const RecipeCard = ({ recipe }) => {
           <div className="content">
             <div className="info">
               <h4>{ title }</h4>
-              <p>Takes approx { cookingTime } mins to make</p>
+              <p>Takes approx { cookingTIme } mins to make</p>
             </div>
             <div className="actions">
               <Link href={`/recipes/${slug}`}>
@@ -25,6 +26,51 @@ const RecipeCard = ({ recipe }) => {
               </Link>
             </div>
           </div>
+
+          <style jsx>{`
+            .card{
+              transform: rotateZ(-1deg);
+            }
+
+            .content {
+              background: #fff;
+              box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+              margin: 0;
+              position: relative;
+              top: -40px;
+              left: -10px;
+            }
+
+            .info {
+              padding: 16px;
+            }
+
+            .info {
+              padding: 16px;
+            }
+
+            .info h4{
+              margin: 4px 0;
+            }
+
+            .info p {
+              margin: 0;
+              color: #777;
+            }
+
+            .actions {
+              margin-top: 20px;
+              display: flex;
+              justify-content: flex-end;
+            }
+
+            .actions a {
+              color: #fff;
+              background: #f01b29;
+              padding: 16px 24px;
+              text-decoration: none;
+            }
+          `}</style>
         </div>
     )
 }
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,3 +1,5 @@
+
+
 export default function RecipeDetails() {
   return (
     <div>
